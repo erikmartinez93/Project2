b6318363cfa9110b297dba6c768fa93e6ce5e691
erikmartinez93 <erkmartinez93@gmail.com>
2022-10-24 22:21:45 -0400

    Initial commit

---
+++ src/Project/WebDriver.java
@@ -0,0 +1,71 @@
+package Project;
+
+public class WebDriver {
+    void openBrowser(){
+        System.out.println("Opening a Browser");
+    }
+    void closeBrowser() {
+        System.out.println("closing the browser");
+    }
+    void navigate(){
+        System.out.println("Locate the url");
+    }
+
+    void GetScreenShot(){
+        System.out.println("Take a ScreenShot");
+
+    }
+}
+class ChromeDriver extends WebDriver {
+
+    void openBrowser(){
+        System.out.println("Opening a Browser");
+    }
+    void closeBrowser() {
+        System.out.println("closing the browser");
+    }
+    void navigate(){
+        System.out.println("Locate the url");
+    }
+
+    void GetScreenShot(){
+        System.out.println("Take a ScreenShot");
+
+    }
+}
+
+class  FireFoxDriver extends WebDriver {
+
+    void openBrowser(){
+        System.out.println("Opening a Browser");
+    }
+    void closeBrowser() {
+        System.out.println("closing the browser");
+    }
+    void navigate(){
+        System.out.println("Locate the url");
+    }
+
+    void GetScreenShot(){
+        System.out.println("Take a ScreenShot");
+    }
+
+}
+
+class SafariDriver extends WebDriver {
+
+    void openBrowser(){
+        System.out.println("Opening a Browser");
+    }
+    void closeBrowser() {
+        System.out.println("closing the browser");
+    }
+    void navigate(){
+        System.out.println("Locate the url");
+    }
+
+    void GetScreenShot(){
+        System.out.println("Take a ScreenShot");
+
+    }
+}
